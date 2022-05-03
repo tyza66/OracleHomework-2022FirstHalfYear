2b652d13c9f73538b0d2a1915d53f3b1f3cd1dcc
洮羱芝闇 <shun_@outlook.com>
2022-05-03 09:13:16 +0800

    UPDATE

--- 代码/jbcbDemo/src/com/sdm/demo/Demo1.java
+++ 代码/jbcbDemo/src/com/sdm/demo/Demo1.java
@@ -8,41 +8,51 @@ import java.sql.Statement;
 
 /**
  * jdbc练习1
- * @author shun_
- * 准备jdk1.8+eclipse环境  java se技术平台
- * 创建Java项目 src文件
+ * 
+ * @author shun_ 准备jdk1.8+eclipse环境 java se技术平台 创建Java项目 src文件
  */
 public class Demo1 {
-//alt+?
-	//jar包拖进src,右键bulid path
-	//注册驱动
-	//建立连接
-	//执行sql语句
-	//关闭资源
+	// alt+?
+	// jar包拖进src,右键bulid path
+	// 注册驱动
+	// 建立连接
+	// 执行sql语句
+	// 关闭资源
 	public static void main(String[] args) {
-			try {
-				Class.forName("oracle.jdbc.driver.OracleDriver");
-				System.out.println("注册成功");
-			} catch (ClassNotFoundException e) {
-				e.printStackTrace();
-				System.out.println("注册失败 检查jar包和驱动名");
-			}
-			try {
-			      Connection con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521/helowin","test","test");
-			      System.out.println("连接成功");
-			      Statement st = con.createStatement();
-			      ResultSet rs = st.executeQuery ("SELECT * FROM Student");	
-			      while (rs.next()) {
-			             int x = rs.getInt("a");
-			             String s = rs.getString("b");
-			      }
-			      rs.close();
-			      st.close();
-			      con.close();
-			} catch (SQLException e) {
-			      e.printStackTrace();
-			      System.out.println("连接失败，请检查url，用户名，密码是否正确，数据库是否启动");
-			}
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			System.out.println("注册成功");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			System.out.println("注册失败 检查jar包和驱动名");
+		}
+		//连接信息
+		String url = "jdbc:oracle:thin:@localhost:1521/helowin";
+		String root = "test";
+		String pwd = "test";
+		//查询信息
+		int bumenId = 11;
+		String bumenName = "A集团";
+		String bumenDiZhi = "胡果山";
+		String sql = "INSERT INTO BUMEN VALUES('"+bumenId+"','"+ bumenName +"','"+ bumenDiZhi +"')";
+		try {
+			Connection con = DriverManager.getConnection(url, root, pwd);
+			System.out.println("连接成功");
+			Statement st = con.createStatement();
+			// 添加
+			// ResultSet rs = st.executeQuery ("SELECT * FROM BUMEN");
+			// while (rs.next()) {
+			// int x = rs.getInt("a");
+			// String s = rs.getString("b");
+			// }
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.out.println("连接失败，请检查url，用户名，密码是否正确，数据库是否启动");
+		} finally {
+			// rs.close();
+			// st.close();
+			// con.close();
+		}
 
 	}
 
