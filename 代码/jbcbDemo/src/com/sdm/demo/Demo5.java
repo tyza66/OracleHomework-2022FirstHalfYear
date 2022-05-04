f6899552f11ea940389d7612534edca6674beb03
洮羱芝闇 <shun_@outlook.com>
2022-05-04 16:25:15 +0800

    update

--- 代码/jbcbDemo/src/com/sdm/demo/Demo5.java
+++ 代码/jbcbDemo/src/com/sdm/demo/Demo5.java
@@ -4,7 +4,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.Statement;
 
 public class Demo5 {
 	public static void main(String[] args) {
