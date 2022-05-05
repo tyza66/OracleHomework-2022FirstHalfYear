515a0e2eebb20c647fdb06853ecee887b91a9b4b
洮羱芝闇 <shun_@outlook.com>
2022-05-05 17:49:22 +0800

    update

--- 代码/jbcbDemo/src/com/sdm/util/BaseDao.java
+++ 代码/jbcbDemo/src/com/sdm/util/BaseDao.java
@@ -13,7 +13,7 @@ public class BaseDao {
 	public static final String PASSWORD = "test";
 	Connection conn = null;
 	PreparedStatement ps = null;
-	
+
 	public Connection getConn() {
 		try {
 			Class.forName(DRIVER);
@@ -25,32 +25,54 @@ public class BaseDao {
 		}
 		return conn;
 	}
-	
+
 	public void closeAll() {
 		try {
-			if(ps!=null) {
+			if (ps != null) {
 				ps.close();
 			}
-			if(conn!=null) {
+			if (conn != null) {
 				conn.close();
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	public int update(String sql,Object[] params) {
+
+	public int update(String sql, Object[] params) {
 		int code = 0;
 		conn = this.getConn();
 		try {
 			ps = conn.prepareStatement(sql);
+			for (int i = 0; i < params.length; i++) {
+				ps.setObject(i + 1, params[i]);
+			}
+			ps.executeUpdate();
 		} catch (SQLException e) {
 			e.printStackTrace();
+		} finally {
+			this.closeAll();
 		}
 		return code;
 	}
-	
-	public ResultSet query() {
-		return null;
+
+	public ResultSet query(String sql, Object[] params) {
+		conn = this.getConn();
+		ResultSet rs = null;
+		try {
+			ps = conn.prepareStatement(sql);
+			if (params != null) {
+				for (int i = 0; i < params.length; i++) {
+					ps.setObject(i + 1, params[i]);
+				}
+			}
+			rs = ps.executeQuery();
+			rs.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			this.closeAll();
+		}
+		return rs;
 	}
 }
