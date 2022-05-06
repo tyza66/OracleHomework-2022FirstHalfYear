b84a8611f2e38d4256bb70ba18f4b4216758b0ae
洮羱芝闇 <shun_@outlook.com>
2022-05-06 09:15:00 +0800

    update

--- 代码/jbcbDemo/src/com/sdm/dao/DepartmrntsDao.java
+++ 代码/jbcbDemo/src/com/sdm/dao/DepartmrntsDao.java
@@ -1,9 +1,9 @@
 package com.sdm.dao;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-
 import com.sdm.entity.Departments;
 import com.sdm.util.BaseDao;
 
@@ -13,10 +13,23 @@ public class DepartmrntsDao {
 	BaseDao dao = new BaseDao();
 
 	public List<Departments> selectAll() {
-		List<Departments> list = new ArrayList();
+		List<Departments> list = new ArrayList<Departments>();
 		String sql = "select * from departments";
 		ResultSet rs = dao.query(sql, null);
-		
+		try {
+			while (rs.next()) {
+				int departmentId = rs.getInt("department_id");
+				String departmentName = rs.getString("department_name");
+				int managerId = rs.getInt("manager_id");
+				int locationId = rs.getInt("location_id");
+				Departments departments = new Departments(departmentId, departmentName, managerId, locationId);
+				list.add(departments);
+			}
+			rs.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return list;
 	}
 
--- 代码/jbcbDemo/src/com/sdm/test/BaseDaoTest.java
+++ 代码/jbcbDemo/src/com/sdm/test/BaseDaoTest.java
@@ -1,22 +1,30 @@
 package com.sdm.test;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import com.sdm.util.BaseDao;
 
 public class BaseDaoTest {
 	public static void main(String[] args) {
 		BaseDao dao = new BaseDao();
-		//测试增加
-//		String sql = "insert into bumen values(?,?,?)";
-//		Object[] params = { 99, "测试部门", "滨海小区" };
-//		int dode = dao.update(sql, params);
-//		System.out.println(dao);
-		//测试查询
-		String sql= "select * from bumen";
+		// 测试增加
+		// String sql = "insert into bumen values(?,?,?)";
+		// Object[] params = { 99, "测试部门", "滨海小区" };
+		// int dode = dao.update(sql, params);
+		// System.out.println(dao);
+		// 测试查询
+		String sql = "select * from bumen";
 		ResultSet rs = dao.query(sql, null);
-		while(rs.next()){
-			
+		try {
+			while (rs.next()) {
+				int bumenId = rs.getInt("bumenid");
+				String bumenName = rs.getString("bumenname");
+				String bumenDiZhi = rs.getString("bumendizhi");
+				System.out.println(bumenId + "," + bumenName + "," + bumenDiZhi);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
 	}
 }
