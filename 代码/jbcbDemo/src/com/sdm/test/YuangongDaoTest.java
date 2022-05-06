c02e3b64e7165fa835e1ed7c6342367801b7149b
洮羱芝闇 <shun_@outlook.com>
2022-05-06 18:22:15 +0800

    update

--- 代码/jbcbDemo/src/com/sdm/test/YuangongDaoTest.java
+++ 代码/jbcbDemo/src/com/sdm/test/YuangongDaoTest.java
@@ -3,10 +3,12 @@ package com.sdm.test;
 import java.util.List;
 
 import com.sdm.dao.YuangongDao;
+import com.sdm.entity.Yuangong;
 
 public class YuangongDaoTest {
 	public static void main(String[] args) {
 		YuangongDao yd = new YuangongDao();
+		yd.insert(new Yuangong(1234,"ll","123",111,"2011-01-01",9999,999,32));
 		List l = yd.selectAll();
 		for(int i = 0;i<l.size();i++) {
 			System.out.println(l.get(i));
