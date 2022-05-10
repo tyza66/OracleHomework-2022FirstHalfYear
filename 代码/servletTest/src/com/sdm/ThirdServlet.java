c84a847ea97bbc8769beac7ac2731fae37eb0f8f
洮羱芝闇 <shun_@outlook.com>
2022-05-10 16:23:37 +0800

    update

---
+++ 代码/servletTest/src/com/sdm/ThirdServlet.java
@@ -0,0 +1,43 @@
+package com.sdm;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ThirdServlet
+ * 练习获取请求参数
+ */
+@WebServlet("/third.do")
+public class ThirdServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ThirdServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//要想获取请求参数
+		String id = request.getParameter("id");
+		System.out.println(id);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
