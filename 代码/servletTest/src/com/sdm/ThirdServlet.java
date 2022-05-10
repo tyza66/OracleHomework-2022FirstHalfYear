5d78862ea18750044750165c7f65b40a0be5231b
洮羱芝闇 <shun_@outlook.com>
2022-05-10 17:11:36 +0800

    update

--- 代码/servletTest/src/com/sdm/ThirdServlet.java
+++ 代码/servletTest/src/com/sdm/ThirdServlet.java
@@ -1,6 +1,8 @@
 package com.sdm;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -27,9 +29,23 @@ public class ThirdServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
 		//要想获取请求参数
 		String id = request.getParameter("id");
-		System.out.println(id);
+		//对业务实现具体逻辑处理
+		String pwd = request.getParameter("pwd");
+		String result = "";
+		if(id.equals("lisi")&&pwd.equals("123")) {
+			result = "登陆成功";
+		}else {
+			result = "登陆失败";
+		}
+		//发送响应数据
+		PrintWriter out = response.getWriter();
+		out.print(result);
+		out.close();
 	}
 
 	/**
