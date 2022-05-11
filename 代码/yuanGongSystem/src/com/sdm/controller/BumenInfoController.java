1ff2cddce7e2d57a0cd65a538b4b29a07a2e2a4b
洮羱芝闇 <shun_@outlook.com>
2022-05-11 18:08:20 +0800

    update

--- 代码/yuanGongSystem/src/com/sdm/controller/BumenInfoController.java
+++ 代码/yuanGongSystem/src/com/sdm/controller/BumenInfoController.java
@@ -1,11 +1,15 @@
 package com.sdm.controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.sdm.service.BumenService;
+import com.sdm.service.imp.BumenServiceImp;
 
 /**
  * Servlet implementation class BumenInfoController
@@ -13,27 +17,35 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/bumen/info.do")
 public class BumenInfoController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public BumenInfoController() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	public BumenInfoController() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("utf-8");
+		response.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		BumenService bumenService = new BumenServiceImp();
+		response.getWriter().print(bumenService.bumenInfo());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
--- 代码/yuanGongSystem/src/com/sdm/service/imp/BumenServiceImp.java
+++ 代码/yuanGongSystem/src/com/sdm/service/imp/BumenServiceImp.java
@@ -3,13 +3,14 @@ package com.sdm.service.imp;
 import java.util.List;
 
 import com.sdm.entity.Bumen;
+import com.sdm.mapper.BumenMapper;
 import com.sdm.service.BumenService;
 
 public class BumenServiceImp implements BumenService {
-
+	BumenMapper bumenMapper = new BumenMapper();
 	@Override
 	public List<Bumen> bumenInfo() {
-		return null;
+		return bumenMapper.selectAll();
 	}
 
 }
