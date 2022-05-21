76655995d2646b08fe56bd0c7d3205ce5307056f
洮羱芝闇 <shun_@outlook.com>
2022-05-21 15:18:56 +0800

    update

--- 作业项目/临时页面/js/control1_main.js
+++ 作业项目/临时页面/js/control1_main.js
@@ -1,7 +1,13 @@
 var app = new Vue({
   el: "#app",
   data: {
-    
+    search: '',
+    tableData: [{
+      id: '1',
+      user: 'tyza66',
+      nickname: '洮羱芝闇',
+      member: '管理员'
+    }]
   },
   created: function () {
 
