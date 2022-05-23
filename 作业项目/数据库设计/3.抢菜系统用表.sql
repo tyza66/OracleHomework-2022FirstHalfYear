e4ef058da0016aea653cf05185717552cbbc7299
洮羱芝闇 <shun_@outlook.com>
2022-05-23 14:32:02 +0800

    update

--- 作业项目/数据库设计/3.抢菜系统用表.sql
+++ 作业项目/数据库设计/3.抢菜系统用表.sql
@@ -1,6 +1,7 @@
  --抢菜表
 CREATE TABLE vegetable(
 vegetable_id number primary key,--id
-vegetable_kinds varchar(255),--素菜准备
-
+vegetable_kinds varchar(255),--蔬菜种类
+vegetable_number number, --剩余量
+vegetable_place varchar(255) --放置地点
 );
