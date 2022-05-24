147e3204245a28f124e557a3efc134cd6cfb306a
洮羱芝闇 <shun_@outlook.com>
2022-05-24 15:56:09 +0800

    update

---
+++ 作业项目/数据库设计/4.交友匹配管理用表.sql
@@ -0,0 +1,15 @@
+CREATE TABLE Friends(
+zid number primary key,
+z1 number(1),
+z2 number(1),
+z3 number(1),
+z4 number(1),
+z5 number(1),
+z6 number(1),
+z7 number(1),
+z8 number(1),
+z9 number(1),
+z10 number(1)
+);
+--各个匹配功能 1是关 2是启动
+INSERT into Friends values(1,1,1,1,1,1,1,1,1,1,1);
